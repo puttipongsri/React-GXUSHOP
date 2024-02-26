987c5c06901857c9493bd2553310b91ea4acc6bf
Puttipong Srisaard <bank0956581633c@gmail.com>
2024-02-26 23:04:08 +0700

    Create Register.js

---
+++ src/login/Register.js
@@ -0,0 +1,117 @@
+import React, { useState } from 'react';
+import './Register.css';
+import { Link } from 'react-router-dom';
+
+const Register = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [address, setAddress] = useState('');
+  const [phonenumber, setPhonenumber] = useState('');
+  const [error, setError] = useState('');
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+
+    if (!username || !password || !firstName || !lastName || !address || !phonenumber) {
+      setError('Please fill in all fields.');
+      return;
+    }
+
+    const newUser = {
+      username: username,
+      password: password,
+      firstName: firstName,
+      lastName: lastName,
+      address: address,
+      phonenumber: phonenumber
+    };
+
+    try {
+      const response = await fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newUser)
+      });
+
+      if (response.ok) {
+        alert('Registration successful');
+        window.location.href = '/'; // Redirect to login page upon successful registration
+      } else {
+        const errorMessage = await response.json();
+        setError(errorMessage.message);
+      }
+    } catch (error) {
+      console.error('Error registering user:', error);
+    }
+  };
+
+  return (
+    <div>
+      <header className={`red-header : ''}`}>
+        <div className="spacer"></div>
+        <h1>
+          <img src="https://cdn.discordapp.com/attachments/1062376070390743123/1210612505286811648/LogoGXUSHOP1.png?ex=65eb31c1&is=65d8bcc1&hm=796439ea811f58cc4ee6874946fc7642359a7c61c5374adc42af609c2bb0be0c&" alt="Descriptive text" className="logo-image" />
+        </h1>
+      </header>
+      <div className="register-container">
+        <h2>Register</h2>
+        <form onSubmit={handleRegister}>
+          <div className="input-group">
+            <input
+              type="text"
+              placeholder="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              className="input-field"
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="input-field"
+            />
+            <input
+              type="text"
+              placeholder="First Name"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+              className="input-field"
+            />
+            <input
+              type="text"
+              placeholder="Last Name"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+              className="input-field"
+            />
+            <input
+              type="text"
+              placeholder="Address"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+              className="input-field"
+            />
+            <input
+              type="text"
+              placeholder="Phone Number"
+              value={phonenumber}
+              onChange={(e) => setPhonenumber(e.target.value)}
+              className="input-field"
+              pattern="\d*"
+            />
+          </div>
+          {error && <p className="error-text">{error}</p>}
+          <Link to="/" className="BacktoLogin-link">Back to Login</Link>
+          <button type="submit" className="register-button">Register</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
