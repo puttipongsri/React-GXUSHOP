ac62672c4dbc6e7f892ac7e5042c3be2ef77bde2
Puttipong Srisaard <bank0956581633c@gmail.com>
2024-02-26 23:03:11 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
+// App.js
 import React from 'react';
-import { BrowserRouter, Route , Switch , Redirect, Routes } from 'react-router-dom';
-import Login from './login'; // Import your Login component
-import Home from './Home'; // Import your Home component
-import Register from './Register';
-import { Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Login from './login/login';
+import Register from './login/Register';
+import Account from './page/account';
+import Home from './page/Home';
+import Setcom from './page/setcom';
+import Setcommoniter from './page/setcommoniter';
+import Fullset from './page/Fullset';
+import ProductPage from './page/ProductPage';
+import Makepayment from './page/Makepayment';
+
 
 const App = () => {
   return (
-    <BrowserRouter>
-  <Routes>
-    <Route path="/" element={<Navigate to="/login" />} />
-    <Route path="/login" element={<Login />} />
-    <Route path="/home" element={<Home />} />
-    <Route path="/register" element={<Register />} />
-  </Routes>
-</BrowserRouter>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/Home" element={<Home />} />
+        <Route path="/account" element={<Account />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/Setcom" element={<Setcom />} />
+        <Route path="/Setcommoniter" element={<Setcommoniter />} />
+        <Route path="/Fullset" element={<Fullset />} />
+        <Route path="/productpage/:id" element={<ProductPage />} />
+        <Route path="/makepayment" element={<Makepayment />} />
 
 
-    
+      </Routes>
+    </Router>
   );
 };
 
